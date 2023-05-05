d24a40ab804ad1587417f9c080281d4b80691060
vanpipy <vanpipy@gmail.com>
2023-05-05 12:16:36 +0800

    chore: fulfill the methods cases

--- test/veslo.spec.ts
+++ test/veslo.spec.ts
@@ -3,7 +3,7 @@ import { expect } from 'chai';
 import Veslo from '../src/Veslo';
 
 describe('Veslo', () => {
-  // const methods = ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS', 'TRACE', 'PATCH'];
+  const methods = ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS', 'TRACE', 'PATCH'];
   const app = new Veslo();
   let i = 0;
 
@@ -34,9 +34,9 @@ describe('Veslo', () => {
       });
   });
 
-  it(`should reply the expected content when hit a get route`, (done) => {
+  it(`should reply the expected content when hit a get method route`, (done) => {
     app.route({
-      path: '/test',
+      path: '/testget',
       method: 'GET',
       stack: [
         ({ res }) => {
@@ -47,7 +47,7 @@ describe('Veslo', () => {
 
     const server = app.run.bind(app);
     void request(server)
-      .get('/test')
+      .get('/testget')
       .expect(200)
       .end((err, res) => {
         if (err) {
@@ -56,6 +56,196 @@ describe('Veslo', () => {
 
         expect(res.text).to.eq('test the route');
 
+        return done();
+      });
+  });
+
+  it(`should reply the expected content when hit a head route`, (done) => {
+    app.route({
+      path: '/testhead',
+      method: 'HEAD',
+      stack: [
+        ({ res }) => {
+          res.end();
+        },
+      ],
+    });
+
+    const server = app.run.bind(app);
+    void request(server)
+      .head('/testhead')
+      .expect(200)
+      .end(() => {
+        return done();
+      });
+  });
+
+  it(`should reply the expected content when hit a post method route`, (done) => {
+    app.route({
+      path: '/testpost',
+      method: 'POST',
+      stack: [
+        ({ res }) => {
+          res.end('test the route');
+        },
+      ],
+    });
+
+    const server = app.run.bind(app);
+    void request(server)
+      .post('/testpost')
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        expect(res.text).to.eq('test the route');
+
+        return done();
+      });
+  });
+
+  it(`should reply the expected content when hit a put route`, (done) => {
+    app.route({
+      path: '/testput',
+      method: 'PUT',
+      stack: [
+        ({ res }) => {
+          res.end('test the route');
+        },
+      ],
+    });
+
+    const server = app.run.bind(app);
+    void request(server)
+      .put('/testput')
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        expect(res.text).to.eq('test the route');
+
+        return done();
+      });
+  });
+
+  it(`should reply the expected content when hit a delete method route`, (done) => {
+    app.route({
+      path: '/testdelete',
+      method: 'DELETE',
+      stack: [
+        ({ res }) => {
+          res.end('test the route');
+        },
+      ],
+    });
+
+    const server = app.run.bind(app);
+    void request(server)
+      .delete('/testdelete')
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        expect(res.text).to.eq('test the route');
+
+        return done();
+      });
+  });
+
+  it(`should reply the expected content when hit a connect method route`, (done) => {
+    app.route({
+      path: '/testconnect',
+      method: 'CONNECT',
+      stack: [
+        ({ res }) => {
+          res.end();
+        },
+      ],
+    });
+
+    const server = app.run.bind(app);
+    void request(server)
+      .connect('/testconnect')
+      .expect(200)
+      .end(() => {
+        return done();
+      });
+  });
+
+  it(`should reply the expected content when hit a options method route`, (done) => {
+    app.route({
+      path: '/testoptions',
+      method: 'OPTIONS',
+      stack: [
+        ({ res }) => {
+          res.end('test the route');
+        },
+      ],
+    });
+
+    const server = app.run.bind(app);
+    void request(server)
+      .options('/testoptions')
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        expect(res.text).to.eq('test the route');
+
+        return done();
+      });
+  });
+
+  it(`should reply the expected content when hit a trace method route`, (done) => {
+    app.route({
+      path: '/testtrace',
+      method: 'TRACE',
+      stack: [
+        ({ res }) => {
+          res.end('test the route');
+        },
+      ],
+    });
+
+    const server = app.run.bind(app);
+    void request(server)
+      .trace('/testtrace')
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        expect(res.text).to.eq('test the route');
+
+        return done();
+      });
+  });
+
+  it(`should reply the expected content when hit a patch method route`, (done) => {
+    app.route({
+      path: '/testpatch',
+      method: 'PATCH',
+      stack: [
+        ({ res }) => {
+          res.end();
+        },
+      ],
+    });
+
+    const server = app.run.bind(app);
+    void request(server)
+      .patch('/testpatch')
+      .expect(200)
+      .end(() => {
         return done();
       });
   });
@@ -73,7 +263,7 @@ describe('Veslo', () => {
 
     const server = app.run.bind(app);
     void request(server)
-      .get('/test')
+      .get('/testget')
       .end((err) => {
         if (err) {
           return done(err);
@@ -88,7 +278,7 @@ describe('Veslo', () => {
   it('should invoke the middlewares when request successfully again', (done) => {
     const server = app.run.bind(app);
     void request(server)
-      .get('/test')
+      .get('/testget')
       .end((err) => {
         if (err) {
           return done(err);
