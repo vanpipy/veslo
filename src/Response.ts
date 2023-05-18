33a8eee85d8acf5f5640d1fd51f344cac1f4d43c
vanpipy <vanpipy@gmail.com>
2023-05-18 10:54:51 +0800

    feat: extend the Response.header and Response.headers

--- src/Response.ts
+++ src/Response.ts
@@ -1,5 +1,23 @@
 import { IncomingMessage, ServerResponse } from 'node:http';
 
-class Response<Request extends IncomingMessage = IncomingMessage> extends ServerResponse<Request> {}
+class Response<Request extends IncomingMessage = IncomingMessage> extends ServerResponse<Request> {
+  header(name: string, value?: string) {
+    if (name && value) {
+      this.setHeader(name, value);
+    } else {
+      return this.getHeader(name);
+    }
+  }
+
+  headers(newHeaders?: Record<string, string | string[]>) {
+    if (newHeaders) {
+      Object.keys(newHeaders).forEach((name) => {
+        this.setHeader(name, newHeaders[name]);
+      });
+    } else {
+      return this.getHeaders();
+    }
+  }
+}
 
 export default Response;
